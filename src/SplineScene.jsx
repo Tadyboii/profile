43e1dbc27b699fa330391d2e0336a8f4a4255259
Tadyboii <thadyboygwapo82@gmail.com>
2024-11-18 17:51:41 +0800

    config

---
+++ src/SplineScene.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineScene() {
+  return (
+    <div className='spline-scene'>
+        <Spline scene="https://prod.spline.design/0rHCV74FZezQFnlG/scene.splinecode" />
+    </div>
+  );
+}
