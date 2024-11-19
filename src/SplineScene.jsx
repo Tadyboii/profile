3642660fc192587f18a70eecd4ad23e7ec2c6d64
Tadyboii <thadyboygwapo82@gmail.com>
2024-11-19 13:10:47 +0800

    change spline scene

--- src/SplineScene.jsx
+++ src/SplineScene.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function SplineScene() {
   return (
     <div className='spline-scene'>
-        <Spline scene="https://prod.spline.design/0rHCV74FZezQFnlG/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/cUbI17njiouq7CZT/scene.splinecode" />
     </div>
   );
 }
